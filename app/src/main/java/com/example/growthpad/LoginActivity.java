2ad964eb2c298d1c36a34e3f1c8b3b5dc22bd604
Mitulpatel7 <mitul71101@gmail.com>
2019-12-31 20:39:26 +0530

    Signup Screen

--- app/src/main/java/com/example/growthpad/LoginActivity.java
+++ app/src/main/java/com/example/growthpad/LoginActivity.java
@@ -1,16 +1,19 @@
 package com.example.growthpad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText login_email , login_password;
-    Button login_btn;
+    TextView forgotpassword ;
+    Button login_btn , createaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,35 @@ public class LoginActivity extends AppCompatActivity {
         login_email = findViewById(R.id.login_emailid);
         login_password  = findViewById(R.id.login_password);
         login_btn = findViewById(R.id.login_btn);
+        forgotpassword = findViewById(R.id.forgot_password);
+        createaccount = findViewById(R.id.create_account_btn);
+
+        //create account click event
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
+
+
+        // frogot password click event
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,Splash_Screen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+        // login button click event
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/growthpad/SignupActivity.java
+++ app/src/main/java/com/example/growthpad/SignupActivity.java
@@ -9,59 +9,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
-    EditText username,phone,password;
-    Button submit,google_singup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
-
-        username = findViewById(R.id.signup_email);
-        phone = findViewById(R.id.signup_phone);
-        password = findViewById(R.id.signup_password);
-        google_singup = findViewById(R.id.google_signup);
-        submit = findViewById(R.id.signup_submit_btn );
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("admin@gmail.com")&& password.getText().toString().equals("123456") && phone.getText().toString().equals("1234567890"))
-                {
-                    Toast.makeText(SignupActivity.this, "Sign Up Successfully", Toast.LENGTH_SHORT).show();
-
-                }
-                else if(username.getText().toString().equals(""))
-                {
-                    username.setError("Email id Required");
-                }
-                else if(password.getText().toString().equals("") | password.length()<6)
-                {
-                    password.setError("Password Cannot be less than 6 Character");
-                }
-                else if (phone.getText().toString().equals("") | phone.length()<10)
-                {
-                    phone.setError("Phone Number Must Be Greater Than 10 Digit");
-                }
-                else
-                {
-                    Toast.makeText(SignupActivity.this, "Sign Up Unsuccessfully", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-
-
-        google_singup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignupActivity.this, "Re-directing To Google Sign Up Page", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
 
 
     }
